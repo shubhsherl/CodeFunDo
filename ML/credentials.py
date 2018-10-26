b1ad86fada61140289d74e313de52ed600295e15
shubhsherl <dhoni.singh094@gmail.com>
2018-10-26 11:03:57 +0530

    move ml data to ML

---
+++ ML/credentials.py
@@ -0,0 +1,6 @@
+# Fill here your own Twitter Application Credentials (see https://apps.twitter.com/)
+
+ACCESS_TOKEN = '1438045896-i8uhzPq2W3p13zdYvFXpKicKPr0wZeTNvU0vmfK'
+ACCESS_SECRET = 'rYnWj9sOxp18CizCgT0S44gd0BbSGJKS1eEmn2JAPws6G'
+CONSUMER_KEY = '8Rsc9NUBULBwsZNLWSb6d9ddK'
+CONSUMER_SECRET = 'foTgqZc6QW3G9DuQkxGK42Uq4yY3zpQmz6ImWkfpSoFE2fzRrb'
